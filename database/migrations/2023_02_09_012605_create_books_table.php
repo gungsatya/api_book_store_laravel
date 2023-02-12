31cc68fe31b84b0003b9cb879579b79e3e71611c
Agung Satya <i.g.b.n.satyawibawa@gmail.com>
2023-02-12 12:45:20 +0800

    rearrange migration

--- database/migrations/2023_02_09_012605_create_books_table.php
+++ database/migrations/2023_02_09_012605_create_books_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,12 +14,15 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('author_id')->nullable();
             $table->string('title');
             $table->text('description')->nullable();
             $table->double('price')->default(0);
             $table->date('release_date')->nullable();
             $table->string('image_path')->nullable();
             $table->timestamps();
+
+            $table->foreign('author_id')->on('authors')->references('id')->cascadeOnDelete();
         });
     }
 
--- database/migrations/2023_02_09_012715_create_book_tag_table.php
+++ database/migrations/2023_02_09_012715_create_book_tag_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,8 +13,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('book_tag', function (Blueprint $table) {
-            $table->foreignId('book_id');
-            $table->foreignId('tag_id');
+            $table->foreignId('book_id')->index();
+            $table->foreignId('tag_id')->index();
+
+            $table->foreign('book_id')->on('books')->references('id')->cascadeOnDelete();
+            $table->foreign('tag_id')->on('tags')->references('id')->cascadeOnDelete();
+
+            $table->primary(['book_id', 'tag_id']);
         });
     }
 
